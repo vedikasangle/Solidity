dc1a9de464e79d927bb020faa9273b4acfdb6f26
Vedika Sangle <87116375+vedikasangle@users.noreply.github.com>
2022-05-15 19:25:47 +0530

    Create statevariable.sol

---
+++ statevariable.sol
@@ -0,0 +1,15 @@
+//SPDX-License-Identifier: UNLICENSED
+pragma solidity >=0.5.0 <0.9.0;
+
+contract demo{
+    uint public age; //age is a state variable
+
+    //constructor(){
+    //    age=100;
+    //}
+
+    function setter() public{
+        age=20;
+    }
+    //statevariable stored at blockchain
+}
